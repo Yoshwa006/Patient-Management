123fd88d53eb319e76f93a8b5342eecb7eb66cf6
yoshwa <ryoshwaa@gmail.com>
2025-03-25 21:18:45 +0530

    Added local files

--- billing-service/src/main/java/com/example/billing_service/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/example/billing_service/grpc/BillingGrpcService.java
@@ -7,16 +7,19 @@ import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
 
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
-
+    List<String> names = new ArrayList<>();
     @Override
     public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> streamObserver){
-        log.info("createBillingAccount request received {}",billingRequest.toString() );
+        names.add((billingRequest.getName()));
+        log.info("createBillingAccount request received {}",names);
         // buisness logic - save in db, calculate etc...
 
         BillingResponse response = BillingResponse.newBuilder()
